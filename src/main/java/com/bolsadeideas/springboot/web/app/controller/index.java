7d01f4f40aec540b2d7cb47c9af91eeedf1b78d0
jas <javier.ant.sanchezc@gmail.com>
2020-08-16 12:52:31 -0500

    first commit

--- src/main/java/com/bolsadeideas/springboot/web/app/controller/index.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controller/index.java
@@ -1,5 +1,8 @@
 package com.bolsadeideas.springboot.web.app.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 
 import org.springframework.http.HttpEntity;
@@ -7,10 +10,18 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bolsadeideas.springboot.web.app.entity.usuario;
+import com.bolsadeideas.springboot.web.app.repository.EmployeeRepository;
 
 
 @Controller
+@RequestMapping
 public class index {
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
 	
 	@GetMapping("/index")
 	public String index() {
@@ -19,4 +30,10 @@ public class index {
 		return "index";
 	}
 
+	
+	  @GetMapping("/usuarios")
+	    public List<usuario> getAllEmployees() {
+	        return employeeRepository.findAll();
+	    }
+	
 }
